ce598ab55fc4204147d848fc6671fa76d26afe05
M-Tereza <mtereza1999@gmail.com>
2026-04-07 07:54:45 -0300

    Create RegraNegocioException.java

---
+++ src/main/java/br/edu/ifsudestemg/sb/exception/RegraNegocioException.java
@@ -0,0 +1,9 @@
+package br.edu.ifsudestemg.sb.exception;
+
+public class RegraNegocioException extends RuntimeException {
+
+    public RegraNegocioException(String msg) {
+        super(msg);
+    }
+
+}
