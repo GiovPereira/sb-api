1a0baa47e59816782d9a25f6cb026a7333a7105d
M-Tereza <mtfnarde@gmail.com>
2026-06-11 04:25:33 -0300

    Atualização de Cliente

--- src/main/java/br/edu/ifsudestemg/sb/api/dto/ClienteDTO.java
+++ src/main/java/br/edu/ifsudestemg/sb/api/dto/ClienteDTO.java
@@ -6,32 +6,91 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 
-import javax.persistence.Entity;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class ClienteDTO {
 
     private Long id;
+
     private String nome;
+
     private String cpf;
+
     private LocalDate dataNascimento;
+
     private String email;
+
     private String telefone;
+
     private String cep;
+
     private String logradouro;
+
     private Integer numero;
+
     private String complemento;
+
     private String bairro;
+
     private String cidade;
+
     private String estado;
 
+    private List<ReservaDTO> reservas;
+
+    private List<EmprestimoDTO> emprestimos;
+
     public static ClienteDTO create(Cliente cliente) {
+
         ModelMapper modelMapper = new ModelMapper();
-        ClienteDTO dto = modelMapper.map(cliente, ClienteDTO.class);
+
+        ClienteDTO dto =
+                modelMapper.map(
+                        cliente,
+                        ClienteDTO.class
+                );
+
+        if (cliente.getReservas() != null) {
+
+            dto.setReservas(
+
+                    cliente.getReservas()
+
+                            .stream()
+
+                            .map(reserva -> {
+
+                                ReservaDTO reservaDTO =
+                                        new ReservaDTO();
+
+                                return reservaDTO.createDTO(reserva);
+
+                            })
+
+                            .collect(Collectors.toList())
+            );
+        }
+
+        if (cliente.getEmprestimos() != null) {
+
+            dto.setEmprestimos(
+
+                    cliente.getEmprestimos()
+
+                            .stream()
+
+                            .map(EmprestimoDTO::create)
+
+                            .collect(Collectors.toList())
+            );
+        }
+
         return dto;
     }
+
 }
--- src/main/java/br/edu/ifsudestemg/sb/model/entity/Cliente.java
+++ src/main/java/br/edu/ifsudestemg/sb/model/entity/Cliente.java
@@ -1,18 +1,19 @@
 package br.edu.ifsudestemg.sb.model.entity;
 
-import javax.persistence.*;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Cliente {
 
     @Id
@@ -20,16 +21,35 @@ public class Cliente {
     private Long id;
 
     private String nome;
+
     private String cpf;
+
     private LocalDate dataNascimento;
+
     private String email;
+
     private String telefone;
+
     private String cep;
+
     private String logradouro;
+
     private Integer numero;
+
     private String complemento;
+
     private String bairro;
+
     private String cidade;
+
     private String estado;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
+    private List<Reserva> reservas = new ArrayList<>();
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
+    private List<Emprestimo> emprestimos = new ArrayList<>();
 
 }
